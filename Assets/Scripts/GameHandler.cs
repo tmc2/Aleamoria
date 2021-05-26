89fc11c7ecafd14b7c49646d507db75740153675
Thiago Chaves <tmc2@cin.ufpe.br>
2021-05-26 18:41:59 -0300

    Added functionalities to instruction screen start game and to GotIt button on playing screen.

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -15,12 +15,29 @@ public class GameHandler : MonoBehaviour
     public GameObject playing_sc;
 
 
-    // Player input screen
+    // Player input screen 
     public TMP_InputField input_field;
     public TMP_Text warning_text;
 
+    // Instruction screen
+    public TMP_Text instruction_text;
+    public TMP_Text Team_text;
 
+    // Playing screen
+    public TMP_Text time;
+    public TMP_Text team1_score_text;
+    public TMP_Text team2_score_text;
+    public TMP_Text aleamoria_text;
+
+    // private variables
     private int players_num = 0;
+    private List<string> full_dataset;
+    private List<string> current_dataset;
+    private int current_idx = 0;
+    private int round = 1;
+    private int team1_score = 0;
+    private int team2_score = 0;
+    private bool team1_is_playing = true;
 
     public void setPlayers()
     {
@@ -31,7 +48,7 @@ public class GameHandler : MonoBehaviour
             players_num = parced_num;
 
             // choose the words for this round
-
+            ChooseWords();
 
             // change screens
             player_input_sc.SetActive(false);
@@ -42,8 +59,60 @@ public class GameHandler : MonoBehaviour
         }
     }
 
+    public void StartGame()
+    {
+        instruction_sc.SetActive(false);
+        GetNextWord();
+        playing_sc.SetActive(true);
+    }
+
+    public void GetNextWord()
+    {
+        if (current_idx < current_dataset.Count)
+        {
+            aleamoria_text.text = current_dataset[current_idx];
+            current_idx += 1;
+        } else
+        {
+            PrepareNextRound();
+        }
+
+    }
+
+    public void AddScore()
+    {
+        if (team1_is_playing)
+        {
+            team1_score += 1;
+            team1_score_text.text = team1_score.ToString();
+        } else
+        {
+            team2_score += 1;
+            team2_score_text.text = team2_score.ToString();
+        }
+    }
+
+    private void PrepareNextRound()
+    {
+
+    }
+
     public void BackToMenu()
     {
         SceneManager.LoadScene("MenuScene");
+    }
+
+    private void ChooseWords()
+    {
+        // randomly get the apropriate ammount of words from the dataset
+        List<string> temp_list = new System.Collections.Generic.List<string>();
+        temp_list.Add("Pudim1");
+        temp_list.Add("Pudim2");
+        temp_list.Add("Pudim3");
+
+        // shuffling the entries
+        //Random rng = new Random();
+        //current_dataset = temp_list.OrderBy(a => rng.Next()).ToList();
+        current_dataset = temp_list;
     }
 }
