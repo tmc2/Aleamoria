f7b1936017435004ea3ee096b5607d481cc1e9c1
Thiago Chaves <tmc2@cin.ufpe.br>
2021-05-26 17:38:58 -0300

    Adding scene change to buttons

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -1,13 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameHandler : MonoBehaviour
 {
-    private int playersNum = 0;
+    // Words dataset
+    TextAsset text_dataset;
 
-    public void setPlayers(int i)
+    // Screens
+    public GameObject player_input_sc;
+    public GameObject instruction_sc;
+    public GameObject playing_sc;
+
+
+    // Player input screen
+    public TMP_InputField input_field;
+    public TMP_Text warning_text;
+
+
+    private int players_num = 0;
+
+    public void setPlayers()
     {
-        playersNum = i;
+        //Debug.Log(input_field.text);
+        int parced_num = int.Parse(input_field.text);
+        if (parced_num > 0)
+        {
+            players_num = parced_num;
+
+            // choose the words for this round
+
+
+            // change screens
+            player_input_sc.SetActive(false);
+            instruction_sc.SetActive(true);
+        } else
+        {
+            warning_text.gameObject.SetActive(true);
+        }
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
     }
 }
