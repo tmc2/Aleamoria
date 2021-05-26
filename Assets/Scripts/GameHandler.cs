8a72a4eae4603b05d19ad30924a5984a20345c5f
Thiago Chaves <tmc2@cin.ufpe.br>
2021-05-26 13:01:02 -0300

    First implementation of main menu

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    private int playersNum = 0;
+
+    public void setPlayers(int i)
+    {
+        playersNum = i;
+    }
+}
