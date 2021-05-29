ec84a66c194e559daafb1ee048326107ac62f65b
Thiago Chaves <tmc2@cin.ufpe.br>
2021-05-29 16:38:47 -0300

    Added an openning animation

---
+++ Assets/Scripts/OpeningBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpeningBehaviour : MonoBehaviour
+{
+    public GameObject menu_screen;
+    public Image background;
+
+    public void Finished()
+    {
+        menu_screen.SetActive(true);
+        background.enabled = true;
+        this.enabled = false;
+    }
+}
