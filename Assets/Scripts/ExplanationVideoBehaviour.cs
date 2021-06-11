3e47c41a171fbe7e503f36ca0115613545f33ba3
Thiago Chaves <tmc2@cin.ufpe.br>
2021-06-10 23:24:09 -0300

    Added video overlay with controllers.

--- Assets/Scripts/ExplanationVideoBehaviour.cs
+++ Assets/Scripts/ExplanationVideoBehaviour.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class ExplanationVideoBehaviour : MonoBehaviour
 {
+    public Image background;
     public GameObject rules_screen;
     public GameObject video_screen;
+    public GameObject video_overlay;
 
     void Start()
     {
         rules_screen.SetActive(false);
+        background.gameObject.SetActive(false);
         var video_player = GetComponent<VideoPlayer>();
         video_player.loopPointReached += EndReached;
     }
@@ -18,6 +22,7 @@ public class ExplanationVideoBehaviour : MonoBehaviour
     void EndReached(UnityEngine.Video.VideoPlayer vp)
     {
         video_screen.SetActive(false);
+        video_overlay.SetActive(false);
         rules_screen.SetActive(true);
     }
 
